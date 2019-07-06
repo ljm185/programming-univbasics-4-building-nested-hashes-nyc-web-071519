a4810f6ca4eecacadaf6f7015e3b713d227f2732
Lourenz Magoncia <ljmagoncia@yahoo.com>
2019-07-06 04:06:41 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -8,8 +8,9 @@ def monopoly_with_second_tier
   monopoly = {
     :railroads => {
       :pieces = 4,
-    :rent_in_dollars => {},
-    :names => {}
+      :rent_in_dollars => {},
+      :names => {}
+    }
   }
 end
 
